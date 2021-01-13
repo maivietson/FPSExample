4ea3393efecc5605b35e011536aa9dbc4c30781b
Sonmv <maivietson.nd@gmail.com>
2021-01-13 22:52:24 +0700

    *[son.mv][Unity3D]: Implement Enemy in scene and add simple AI for enemy

---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float characterRange = 5.0f;
+
+    private bool _alive;
+
+    private void Start()
+    {
+        _alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                if (hit.distance < characterRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+}
